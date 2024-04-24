2d8fa79a4e2dec63a2b18e7561485e338eafcdcc
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-23 21:01:32 -0300

    Propriedades da biblioteca Path

--- path.py
+++ path.py
@@ -2,3 +2,23 @@ from pathlib import Path
 
 p1 = Path('dados', 'texto.txt')
 print(p1)
+
+# Verificar o tipo do arquivo
+
+print(type(p1))
+
+#  Verificando o nome
+
+print(p1.name)
+
+# Pega apenas o nome do arquivo
+
+print(p1.stem)
+
+#  Pega apenas o tipo do arquivo
+
+print(p1.suffix)
+
+# Verifica se o arquivo existe
+
+print(p1.exists)
