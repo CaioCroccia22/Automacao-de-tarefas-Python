ef328b07dc0e49cadf311284cab4a288e91a9c2c
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-25 16:15:06 -0300

    Renameando os arquivos com o nome da pasta antes

--- rename.py
+++ rename.py
@@ -16,5 +16,10 @@ root_dir = Path('files')
 file_paths = root_dir.glob('**/*')
 for path in file_paths:
     if path.is_file():
-        print(path)
-        print(path.parts[-2])
+        # print(path)
+        # print(path.parts[-2])
+        parent_folder = path.parts[-2]
+        new_filename = f'{parent_folder}-{path.name}'
+        newfilepath = path.with_name(new_filename)
+        path.rename(new_filename)
+        
