3ee43de6cbf39cb7761b4a19dbddfa709a34a571
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-26 19:55:06 -0300

    Trocando o nome de arquivos com a data na frente

---
+++ date_files2.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from datetime import datetime
+
+root_dir = Path('files')
+
+for path in root_dir.glob('**/*'):
+    if path.is_file():
+        stats = path.stat()
+        # print(stats)
+        second_created = stats.st_ctime
+        date_created = datetime.fromtimestamp(second_created)
+        date_created_str = date_created.strftime('%Y-%m-%d_%H_%M_%S')
+        # print(date_created_str)
+        
+        # estabelecendo uma variavel com a data -- nome do arquivo
+        new_filename = f'{date_created_str}--{path.name}'
+        # print(new_filename)
+        
+        # Mudando os nomes dos arquivos com a data e hora
+        new_filepath = path.with_name(new_filename)
+        path.rename(new_filepath)
+        
