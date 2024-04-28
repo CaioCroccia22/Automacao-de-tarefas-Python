7bbb93fe6f06c624bb873fe0e89115ea4a36613a
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-28 09:18:08 -0300

    Organizando os sufixos em uma lista

--- exercicio1.py
+++ exercicio1.py
@@ -1,12 +1,31 @@
 from pathlib import Path
 
+
+list_file = []
+
 root_dir = Path('exer_folder')
 filepath = root_dir.iterdir()
 for path in filepath:
     if path.is_file():
-        print(path)
+        # print(path)
         
         file_extension = path.suffix.lower()
-        print(file_extension)
+        # print(file_extension)
         
+        if file_extension == ".zip":
+            list_file.append(path)
+            
+            
+        if file_extension == ".csv":
+            list_file.append(path)
+            
+            
+        if file_extension == ".Ink":
+            list_file.append(path)
+          
+            
+        if file_extension == ".png":
+            list_file.append(path)
+           
+print(list_file)
     
