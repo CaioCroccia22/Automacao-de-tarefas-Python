0ee00b1d827d2fa9a9e04f44166087027ea039a9
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-14 20:10:55 -0300

    Explorando a biblioteca pyPDF2

---
+++ pdf.py
@@ -0,0 +1,24 @@
+import PyPDF2 as pdf
+from PyPDF2 import PdfReader
+
+
+# 1 - Versão e Métodos dessa biblioteca
+# print(pdf.__version__)
+
+
+# 2 - Obter metodos disponiveis
+# print(dir(pdf))
+
+# 3 - Importando o arquivo PDF
+file = open('files/Diploma.pdf', 'rb')
+reader = PdfReader(file)
+print(reader)
+print(reader.metadata)
+
+info = reader.metadata
+
+# 4 - Extraindo algumas informações
+print(info.title)
+print(info.author)
+print(info.creator)
+print(info.subject)
