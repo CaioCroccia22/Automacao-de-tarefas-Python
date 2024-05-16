3b05891eba72cf9246f9bcb99d1e31ad65efadd7
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-15 23:06:59 -0300

    Extraindo textos e metadados do pdf com o metodo exctract_text_from_pdf

--- pdf_operations.py
+++ pdf_operations.py
@@ -2,10 +2,23 @@ import PyPDF2 as pdf
 from PyPDF2 import PdfReader
 
 def get_pdf_metadata(pdf_path):
-     with open('pdf_path', 'rb') as f:
+     with open(pdf_path, 'rb') as f:
          reader = PdfReader(f)
          info = reader.metadata
      return info
  
- 
- 
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as f:
+        reader = PdfReader(f)
+        results = []
+        for i in range(0, len(reader.pages)):
+            selected_pages = reader.pages[i]
+            text = selected_pages.extract_text()
+            results.append(text)
+        return ' '.join(results)
+
+
+# 1 - Buscando Dados e Metadados de um arquivo PDF
+print(get_pdf_metadata('files/Diploma.pdf'))
+print(get_pdf_metadata('files/Diploma.pdf'))
+print(extract_text_from_pdf('files/Diploma.pdf'))
