307220e958da3e6ca270eabcfea339ffff617974
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-22 20:33:16 -0300

    feat: Open calculator with pyautogui

--- teclado_pyautogui.py
+++ teclado_pyautogui.py
@@ -2,4 +2,12 @@ import pyautogui
 import time
 
 
+# Abrindo a calculadora
 pyautogui.press('winleft')
+time.sleep(1)
+pyautogui.write('calculadora')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.hotkey('crtl', 'shift', 'esc')
+
+# Utilizando o teclado virtual
