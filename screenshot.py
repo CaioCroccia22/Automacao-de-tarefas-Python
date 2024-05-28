bf4900f0add2cd4ebf299916474d1ec662803f15
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-27 23:03:10 -0300

    fix: taking screenshot with pyautogui

---
+++ screenshot.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+
+
+# pyautogui.screenshot('exemplo.png')
+pyautogui.moveTo(1751,51, 1)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.screenshot('exemplo2.png')
