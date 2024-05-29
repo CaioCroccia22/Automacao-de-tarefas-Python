b430167d8dac21020d1b48a4ba1b6aefe1a2e86e
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-29 20:58:40 -0300

    fix: switch time to pyautogui

--- youtube.py
+++ youtube.py
@@ -3,11 +3,11 @@ import time
 
 pyautogui.press('winleft')
 time.sleep(1)
-time.write('opera')
+pyautogui.write('chrome')
 time.sleep(1)
 pyautogui.press('enter')
 time.sleep(1)
 pyautogui.click(1510,189, duration=1)
-pyautogui.write('youtube')
+pyautogui.write('youtube.com')
 time.sleep(1)
 pyautogui.press('enter')
