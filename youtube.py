cab7cad049676f61c340a6b8d907d35cbd12a68c
caiocroccia22 <caiocroccia22@gmail.com>
2024-05-29 20:56:47 -0300

    feat: searching youtube

--- youtube.py
+++ youtube.py
@@ -3,3 +3,11 @@ import time
 
 pyautogui.press('winleft')
 time.sleep(1)
+time.write('opera')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.click(1510,189, duration=1)
+pyautogui.write('youtube')
+time.sleep(1)
+pyautogui.press('enter')
